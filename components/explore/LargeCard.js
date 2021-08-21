cebdbd3cb43882a0ec0f38dfbd602c521f033563
BicksonNing <bicksonmeitei2@gmail.com>
2021-08-21 19:45:40 +0530

    date picker

--- components/explore/LargeCard.js
+++ components/explore/LargeCard.js
@@ -2,26 +2,28 @@ import Image from "next/image";
 
 const LargeCard = ({ img, title, description, buttonText }) => {
   return (
-    <section className='relative py-16 cursor-pointer '>
-      <div className='relative h-96 min-w-[300px] '>
-        <Image
-          className='rounded-lg'
-          src={img}
-          layout='fill'
-          objectFit='cover'
-        />
-      </div>
+    <section className=' py-16 cursor-pointer '>
+      <div className='relative hover:scale-105 transition transform duration-300 ease-in-out'>
+        <div className='relative h-96 min-w-[300px] '>
+          <Image
+            className='rounded-lg'
+            src={img}
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
 
-      <div className='absolute top-40 md:top-36 left-12'>
-        <h3 className='text-3xl md:text-5xl mb-3 w-64 text-white font-semibold'>
-          {title}
-        </h3>
-        <p className=' text-white text-md md:text-xl w-40 md:w-80'>
-          {description}
-        </p>
-        <button className='bg-white px-4 md:px-8 py-2 md:py-3 rounded-lg font-semibold mt-5 md:mt-10'>
-          {buttonText}
-        </button>
+        <div className='absolute top-20 md:top-20 left-12'>
+          <h3 className='text-3xl md:text-5xl mb-3 w-64 text-white font-semibold'>
+            {title}
+          </h3>
+          <p className=' text-white text-md md:text-xl w-40 md:w-80'>
+            {description}
+          </p>
+          <button className='bg-white px-4 md:px-8 py-2 md:py-3 rounded-lg font-semibold mt-5 md:mt-10'>
+            {buttonText}
+          </button>
+        </div>
       </div>
     </section>
   );
--- components/header/Header.js
+++ components/header/Header.js
@@ -5,8 +5,30 @@ import {
   MenuIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange, DateRangePicker } from "react-date-range";
+import { useMediaQuery } from "@react-hook/media-query";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const isSmallScreen = useMediaQuery("(max-width: 38rem)");
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className='sticky top-0 z-50 grid md:grid-cols-3 bg-white md:shadow-md px-5 py-4 md:px-10'>
       {/* left */}
@@ -19,8 +41,10 @@ function Header() {
         />
       </div>
       {/* center */}
-      <div className='flex items-center border-2 bg-white rounded-full py-1 px-2 md:shadow-sm hover:shadow-md cursor-pointer'>
+      <div className='flex col-span-3 md:col-span-1 items-center border-2 bg-white rounded-full py-1 px-2 md:shadow-sm hover:shadow-md cursor-pointer'>
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type='text'
           placeholder='Start your Search'
           className='flex-grow outline-none bg-transparent px-3 text-sm text-gray-600 placeholder-gray-400 cursor-pointer'
@@ -39,6 +63,28 @@ function Header() {
           <UserCircleIcon className='h-8' />
         </div>
       </div>
+
+      {searchInput && (
+        <div className='py-5 flex-col col-span-3 mx-auto'>
+          <div>
+            {isSmallScreen ? (
+              <DateRange
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect}
+              />
+            ) : (
+              <DateRangePicker
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect}
+              />
+            )}
+          </div>
+        </div>
+      )}
     </header>
   );
 }
